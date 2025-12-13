a21a2321ea083468d014cfa040a5226e8e193f4a
Thao <thao@email.com>
2025-12-13 12:19:34 +0700

    feat: Update UserListDto to include authProviderId from API response

--- types/user.d.ts
+++ types/user.d.ts
@@ -1,5 +1,18 @@
+import { BaseSearchQuery } from './common'
+
 export interface UserCheckResultDto {
   userId: string;
   userName: string;
   fullName: string;
+}
+
+export interface UserListDto {
+  id: string;
+  fullName: string;
+  userName: string;
+  email?: string;
+  authProviderId: string;
+}
+
+export interface SearchUsersQuery extends BaseSearchQuery {
 }
